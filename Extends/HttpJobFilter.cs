8cc42c42cd418f1e30226a597337bc88a99726c9
payneqin <payneqin@qt-asia.com>
2019-07-08 15:26:35 +0800

    add logfilter

--- Extends/HttpJobFilter.cs
+++ Extends/HttpJobFilter.cs
@@ -1,32 +1,35 @@
-﻿using Hangfire.Common;
-using Hangfire.States;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Hangfire.Storage;
+using Hangfire.Common;
 using Hangfire.Logging;
+using Hangfire.States;
+using Hangfire.Storage;
 
-namespace AsyncHttpJob.Extends
-{
-    public class HttpJobFilter: JobFilterAttribute, IApplyStateFilter
-    {
-        private static readonly ILog Logger = LogProvider.GetCurrentClassLogger();
+namespace AsyncHttpJob.Extends {
+    public class HttpJobFilter : JobFilterAttribute, IApplyStateFilter {
+        private static readonly ILog Logger = LogProvider.GetCurrentClassLogger ();
 
-        public void OnStateApplied(ApplyStateContext context, IWriteOnlyTransaction transaction)
-        {
-            var failedState = context.NewState as FailedState;
-            if (failedState != null)
-            {
-                Logger.ErrorException(
-                    String.Format("Background Job #{0} 执行失败。", context.JobId),
-                    failedState.Exception);
+        public void OnStateApplied (ApplyStateContext context, IWriteOnlyTransaction transaction) {
+            if (context.NewState is FailedState) {
+                var failedState = context.NewState as FailedState;
+                if (failedState != null) {
+                    Logger.ErrorException (
+                        String.Format ("Background Job #{0} 执行失败。", context.BackgroundJob.Id),
+                        failedState.Exception);
+                }
+            } else {
+                Logger.InfoFormat (
+                    "当前执行的Job为：#{0}, 状态为：{1}。",
+                    context.BackgroundJob.Id,
+                    context.NewState.Name
+                );
             }
         }
 
-        public void OnStateUnapplied(ApplyStateContext context, IWriteOnlyTransaction transaction)
-        {
-            
+        public void OnStateUnapplied (ApplyStateContext context, IWriteOnlyTransaction transaction) {
+
         }
     }
 }
